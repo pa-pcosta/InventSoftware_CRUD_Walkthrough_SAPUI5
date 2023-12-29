85e89f1bd5c4312ecdf06fd136b2522fae8d0ddd
PA <pedro.augusto@inventsoftware.com.br>
2023-12-29 12:09:02 -0300

    Adicionado controle OpenUI5 no arquivo index.js para mostrar texto no navegador

--- webapp/index.js
+++ webapp/index.js
@@ -1,4 +1,9 @@
-sap.ui.define([], () => {
-    "use strict";
-    alert("UI5 is ready");
+sap.ui.define([
+	"sap/m/Text"
+], (Text) => {
+	"use strict";
+
+	new Text({
+		text: "Hello World"
+	}).placeAt("content");
 });
